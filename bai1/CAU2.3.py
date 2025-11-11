cb1593edefd71b336f4c3607bf36cdeb30ca4a46
TRANVANTRUNG206 <trung12341ff@gmail.com>
2025-11-11 12:24:47 +0700

    Delete bai1 directory

--- bai1/CAU2.3.py
+++
@@ -1,9 +0,0 @@
-print("sinh viên: TRAN VAN TRUNG")
-print("MSV:245752021610133")
-n=int(input("Enter a number"))
-if n % 2 == 0:
-    print ("EVENN Number");
-else:
-    print ("ODD Number");
-
-
--- bai1/CAU2.4.py
+++
@@ -1,7 +0,0 @@
-print("sinh viên: TRAN VAN TRUNG")
-print("MSV:245752021610133")
-i=1;
-for j in range(2,10):
-    print("i:",i,"j:",j)
-    print(i,"/",j)
-    print (i/j);
