4c47007ff9fc1d2002cca7a4a24c2c18b2af4d5e
TRANVANTRUNG206 <trung12341ff@gmail.com>
2025-11-11 12:30:02 +0700

    Add files via upload

---
+++ CAU2.5.py
@@ -0,0 +1,6 @@
+print("sinh viên: TRAN VAN TRUNG")
+print("MSV:245752021610133")
+n = int(input("Enter A Number--->"));
+while n >=0:
+    print (n);
+    n= n - 1;
---
+++ CAU2.9.py
@@ -0,0 +1,8 @@
+print("sinh viên: TRAN VAN TRUNG")
+print("MSV:245752021610133")
+str=input("enter a string:")
+dict ={}
+for i in str:
+    dict[i] = str.count(i)
+    
+print (dict)
